b60e41adb95edecd7a23c8815bf978c4220d2bdf
Fabien Antony <toxy2k@gmail.com>
2020-12-13 22:05:06 +0100

    mise a jour user et session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :session_recuperation
+
+    def session_recuperation 
+        if session[:user_token] != nil
+            user = User.find_by(id: session[:user_token])
+            @current_user = user
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :contents
+  resources :articles
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/sign_in', to: 'session#index_sign_in'
+  post '/sign_in', to: 'session#sign_in'
+  delete '/sign_out', to: 'session#sign_out'
+  
+
 end
